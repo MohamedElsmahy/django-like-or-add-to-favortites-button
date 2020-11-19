8040ef23db98a42684c9b8d79bca29c82c35c90d
MohamedElsmahy <66690245+MohamedElsmahy@users.noreply.github.com>
2020-11-19 06:59:17 +0200

    add the viwe and model for the product

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from product import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('product/<int:id>',views.product_detail , name = 'detail'),
+    path('user/favourites',views.user_favourites , name = 'user_favourites'),
+    path('product/<int:id>/like_or_dislike',views.like_or_unlike , name = 'like'),
 ]
